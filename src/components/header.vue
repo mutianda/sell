5e836081d894a8fb0a0f372621bb44fc22cb8f01
mutianda <qingyi.zongbu@qq.com>
2018-12-05 16:40:46 +0800

    新增

---
+++ src/components/header.vue
@@ -0,0 +1,13 @@
+<template>
+<div>header</div>
+</template>
+<script>
+export default {
+  name: '',
+  data () {
+  }
+}
+</script>
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Header from '@/components/header'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/header',
+      name: 'header',
+      component: Header
+    },
     {
       path: '/',
       name: 'HelloWorld',
